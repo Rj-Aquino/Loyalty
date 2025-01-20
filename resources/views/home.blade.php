453c9aaa9b5f28a994f3ae255955d7513a8de9b2
JM-Garces <163490199+JM-Garces@users.noreply.github.com>
2025-01-20 21:14:01 +0800

    added css

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,24 +5,28 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Home Page</title>
-    <link rel="stylesheet" href="/css/style.css">
+    <link rel="stylesheet" href="/css/home.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 <body>
     
-    <div class="main_header">
+    <div class="main_header d-flex flex-column justify-content-center align-items-center">
+        <div class="store_logo">
+            <img src="Picture/StoreLogo.png" alt="Store Logo" class="img-fluid">
+        </div>
         <h1 class="main_header_text">Dipensa Teknolohiya Grocery</h1>
     </div>
 
-    <div id="main_container" class="d-flex flex-column justify-content-center align-items-center vh-100">
-
+    <div id="main_container" class="d-flex flex-column justify-content-center align-items-center">
+        
         {{-- div for "Welcome Customers" --}}
-        <div class="secondary_header mb-4">
-            <h2>Welcome Loyal Customers</h2>
+        <div class="welcome" style="font-size: 4rem; color: #d6af68; font-weight: bold;">
+            WELCOME LOYAL CUSTOMERS
         </div>
+
     
         {{-- div for "Apply for a Membership" button --}}
-        <div class="container mb-3">
+        <div class="container">
             <a href="/apply">
                 <button class="btn btn-success btn-lg rounded-pill px-5 py-3">APPLY FOR A MEMBERSHIP</button>
             </a>
